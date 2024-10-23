6fc929af6dab71dc5a280f5d3a58bcbb37b4c790
rud-roo <saharudro15@gmail.com>
2024-10-23 23:49:15 +0600

    Learn about the useReducer hook in react

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,15 @@ import App from './App.jsx'
 import './index.css'
 import add from './demo.jsx'
 import StudentProvider from './components/contexts/Student.jsx'
+import App2 from './App2.jsx'
 
 const root =ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   // <StrictMode>
-    <StudentProvider>
-      <App />
-    </StudentProvider>
+    // <StudentProvider>
+    //   <App />
+    // </StudentProvider>
+    <App2/>
   // </StrictMode>,
 )
