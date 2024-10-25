d3a3c8ea1ea3302418dc9c869cef141f205a3025
rud-roo <saharudro15@gmail.com>
2024-10-26 00:09:56 +0600

    Learn more about useReducer hook in react
    Learn more about useReducer hook in react

--- src/components/AllStudentList.jsx
+++ src/components/AllStudentList.jsx
@@ -1,8 +1,11 @@
-import React, { useContext } from 'react'
-import { StudentContext } from './contexts/Student';
+/* eslint-disable react/prop-types */
+import { useContext } from "react";
+import { StudentContext } from "./contexts/Student";
 
 const AllStudentList = () => {
+	const { studentStates, dispatchStudentAction } = useContext(StudentContext);
 
+<<<<<<< Updated upstream
   const {
     studentStates,
     dispatchStudentAction,
@@ -72,3 +75,66 @@ const AllStudentList = () => {
 }
 
 export default AllStudentList
+=======
+	return (
+		<div className="list all-list">
+			<h2>All Students</h2>
+			<ul>
+				{studentStates.students.map((student) => (
+					<li className="list-item" key={student.id}>
+						<span>{student.name}</span>
+						<button
+							onClick={() =>
+								dispatchStudentAction({
+									type: "EDIT_STUDENT",
+									payload: student,
+								})
+							}
+						>
+							Edit
+						</button>
+						<button
+							onClick={() =>
+								dispatchStudentAction({
+									type: "REMOVE_STUDENT",
+									payload: student.id,
+								})
+							}
+						>
+							Remove
+						</button>
+						<button
+							onClick={() =>
+								dispatchStudentAction({
+									type: "UPDATE_PRESENT_STATUS",
+									payload: {
+										id: student.id,
+										isPresent: true,
+									},
+								})
+							}
+						>
+							Send to Present List
+						</button>
+						<button
+							onClick={() =>
+								dispatchStudentAction({
+									type: "UPDATE_PRESENT_STATUS",
+									payload: {
+										id: student.id,
+										isPresent: false,
+									},
+								})
+							}
+						>
+							Send to Absent List
+						</button>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+};
+
+export default AllStudentList;
+>>>>>>> Stashed changes
--- src/components/StudentForm.jsx
+++ src/components/StudentForm.jsx
@@ -1,15 +1,39 @@
-import React, { useContext } from 'react'
-import { StudentContext } from './contexts/Student';
+import { useContext } from "react";
+import { StudentContext } from "./contexts/Student";
 
 
 const StudentForm = () => {
+<<<<<<< Updated upstream
   const {
     studentStates,
     dispatchStudentAction,
     submitHandler,
   } = useContext(StudentContext);
+=======
+	const { studentStates, dispatchStudentAction, submitHandler } =
+		useContext(StudentContext);
+>>>>>>> Stashed changes
 
+	return (
+		<form onSubmit={submitHandler} className="form">
+			<input
+				type="text"
+				value={studentStates.studentName}
+				onChange={(e) =>
+					dispatchStudentAction({
+						type: "CHANGE_STUDENT_NAME",
+						payload: e.target.value,
+					})
+				}
+			/>
+			<button type="submit">
+				{studentStates.editMode ? "Update Student" : "Create Student"}
+			</button>
+		</form>
+	);
+};
 
+<<<<<<< Updated upstream
 
   return (
     <form  className="form" onSubmit={submitHandler}>
@@ -29,3 +53,6 @@ const StudentForm = () => {
 }
 
 export default StudentForm
+=======
+export default StudentForm;
+>>>>>>> Stashed changes
