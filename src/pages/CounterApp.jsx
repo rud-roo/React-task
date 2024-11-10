c85224f1721b1a1b4bc14aae0398f29e08bf08c0
Rudro Saha <saharudro15@gmail.com>
2024-11-10 16:18:19 +0600

    Learn about memo Function and useCallback hook in React

--- src/pages/CounterApp.jsx
+++ src/pages/CounterApp.jsx
@@ -1,15 +1,32 @@
+import { useCallback, useState } from "react";
 import { useCounter } from "../hooks/counterHook";
+import Button from "../components/Button";
+import Title from "../components/title";
 
 const CounterApp = () => {
-	const { counter, increaseHandler, decreaseHandler } = useCounter();
+	console.log("Rendering counter app.")
+	const [counter, setCounter] = useState(0);
+	const [counter2, setCounter2] = useState(0);
+	const [counter3, setCounter3] = useState(0);
+
+	const increaseHandler = useCallback(() => {
+		setCounter((currVal) => currVal+1);
+	}, [])
+	const increaseHandler2 = useCallback(() => {
+		setCounter2((currVal) => currVal+1)
+	}, )
+
 	return (
 		<div>
+			<Title/>
 			<p>The value of the counter is {counter}</p>
-			<button onClick={() => increaseHandler(1)}>Increase By 1</button>
-			<button onClick={() => increaseHandler(5)}>Increase By 5</button>
-
-			<button onClick={() => decreaseHandler(1)}>decrease By 1</button>
-			<button onClick={() => decreaseHandler(3)}>decrease By 3</button>
+			<Button clickHandler={increaseHandler} />
+			<hr />
+			<p>The value of the counter is {counter2}</p>
+			<Button clickHandler={increaseHandler2} />
+			<hr />
+			<p>The value of the counter is {counter3}</p>
+			<button onClick={() => setCounter3(counter3+10)}>Increase by 10</button>
 		</div>
 	);
 };
