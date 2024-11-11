e192f09025826a61d6d09228e91d00f56c412889
Rudro Saha <saharudro15@gmail.com>
2024-11-11 10:25:53 +0600

    Learn the fundamental topics of Redux like store, reducers, devtool, etc.

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,11 @@
 import { createStore, combineReducers } from 'redux'
 import { counterReducer } from './reducer/counter';
 import { cartReducer } from './reducer/cart';
+import { composeWithDevTools } from '@redux-devtools/extension';
 
 const rootReducer = combineReducers({
   counter: counterReducer,
   cart: cartReducer
 })
 
-export const store = createStore(rootReducer);
+export const store = createStore(rootReducer, composeWithDevTools());
