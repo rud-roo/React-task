67a0b3c0d7150a94154bb3578b59e0d5c7b4dce3
Rudro Saha <saharudro15@gmail.com>
2024-11-11 15:36:33 +0600

    Learn about configureStore, createReducer, createAction, createSlice, etc in Redux Toolkit

--- src/store/action-creator/cart.js
+++ src/store/action-creator/cart.js
@@ -1,26 +1,44 @@
-export const addToCart = (product) => {
-  return {
-    type: "ADD_TO_CART",
-    payload: product,
-  }
-}
+// export const addToCart = (product) => {
+//   return {
+//     type: "ADD_TO_CART",
+//     payload: product,
+//   }
+// }
 
-export const removeAnItemFromCart =  (productId) => {
-  return{
-    type: "REMOVE_ITEM_FROM_CART",
-    payload: productId
-  }
-}
+// export const removeAnItemFromCart =  (productId) => {
+//   return{
+//     type: "REMOVE_ITEM_FROM_CART",
+//     payload: productId
+//   }
+// }
 
-export const modifyQuantityOfAnItem = (obj) => {
-  return {
-    type: "MODIFY_QUANTITY_OF_AN_ITEM",
-    payload: obj,
-  }
-}
+// export const modifyQuantityOfAnItem = (obj) => {
+//   return {
+//     type: "MODIFY_QUANTITY_OF_AN_ITEM",
+//     payload: obj,
+//   }
+// }
 
-export const clearCart = () => {
-  return {
-    type: "CLEAR_CART"
-  }
-}
+// export const clearCart = () => {
+//   return {
+//     type: "CLEAR_CART"
+//   }
+// }
+
+
+
+// import { createAction } from "@reduxjs/toolkit";
+
+// export const addToCart = createAction("ADD_TO_CART");
+// export const removeAnItemFromCart = createAction("REMOVE_ITEM_FROM_CART");
+// export const modifyQuantityOfAnItem = createAction("MODIFY_QUANTITY_OF_AN_ITEM");
+// export const clearCart = createAction("CLEAR_CART");
+
+import {
+  addToCart,
+  removeAnItemFromCart,
+  modifyQuantityOfAnItem,
+  clearCart
+} from '../reducer/cart'
+
+export {addToCart, removeAnItemFromCart, modifyQuantityOfAnItem, clearCart}
--- src/store/reducer/cart.js
+++ src/store/reducer/cart.js
@@ -1,34 +1,99 @@
-export const cartReducer = (state = [], action) => {
-  switch(action.type){
-    case "ADD_TO_CART":{
-      const product = state.find((item)=> item.id===action.payload.id);
-      return product
-        ? state.map((item) => {
-          if(item.id === action.payload.id){
-            return {...item, quantity: item.quantity+1}
-          }
-          return item;
-          })
-        : [...state,  {...action.payload, quantity: 1}];
+// export const cartReducer = (state = [], action) => {
+//   switch(action.type){
+//     case "ADD_TO_CART":{
+//       const product = state.find((item)=> item.id===action.payload.id);
+//       return product
+//         ? state.map((item) => {
+//           if(item.id === action.payload.id){
+//             return {...item, quantity: item.quantity+1}
+//           }
+//           return item;
+//           })
+//         : [...state,  {...action.payload, quantity: 1}];
 
-    }
-    case "REMOVE_ITEM_FROM_CART":{
+//     }
+//     case "REMOVE_ITEM_FROM_CART":{
+//       return state.filter((item) => item.id !== action.payload);
+//     }
+//     case "MODIFY_QUANTITY_OF_AN_ITEM":{
+//       const updatedState = state.map((item) => {
+// 				if (item.id === action.payload.id) {
+// 					return { ...item, quantity: action.payload.quantity };
+// 				}
+// 				return item;
+// 			});
+// 			return updatedState;
+//     }
+//     case "CLEAR_CART":{
+//       return []
+//     }
+//     default:{
+//       return state;
+//     }
+//   }
+// }
+
+
+// import { createReducer } from "@reduxjs/toolkit";
+// import { 
+//   addToCart,
+//   removeAnItemFromCart,
+//   modifyQuantityOfAnItem,
+//   clearCart
+//  } from "../action-creator/cart";
+
+// export const cartReducer = createReducer([], (builder) => {
+//   builder
+//     .addCase(addToCart.type, (state, action) => {
+//       const product = state.find((item)=> item.id===action.payload.id);
+//       product
+//         ? product.quantity++ 
+//         : state.push({...action.payload, quantity:1})
+//     })
+//     .addCase(removeAnItemFromCart.type, (state, action) => {
+//       return state.filter((item) => item.id !== action.payload);
+//     })
+//     .addCase(modifyQuantityOfAnItem, (state, action) => {
+//       const product = state.filter((item) => item.id===action.payload.id);
+//       product.quantity = action.payload.quantity;
+//     })
+//     .addCase(clearCart, () => {
+//       return []
+//     })
+// })
+
+
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: [],
+  reducers: {
+    addToCart(state, action){
+      const product = state.find((item)=> item.id===action.payload.id);
+      product
+        ? product.quantity++ 
+        : state.push({...action.payload, quantity:1})
+    },
+    removeAnItemFromCart(state, action){
       return state.filter((item) => item.id !== action.payload);
-    }
-    case "MODIFY_QUANTITY_OF_AN_ITEM":{
-      const updatedState = state.map((item) => {
-				if (item.id === action.payload.id) {
-					return { ...item, quantity: action.payload.quantity };
-				}
-				return item;
-			});
-			return updatedState;
-    }
-    case "CLEAR_CART":{
+    },
+    modifyQuantityOfAnItem(state, action){
+      const product = state.find((item) => item.id===action.payload.id);
+      product.quantity = action.payload.quantity;
+    },
+    clearCart(){
       return []
     }
-    default:{
-      return state;
-    }
   }
-}
+})
+
+export const {
+  addToCart,
+  removeAnItemFromCart,
+  modifyQuantityOfAnItem,
+  clearCart
+} = cartSlice.actions;
+
+
+export default cartSlice;
