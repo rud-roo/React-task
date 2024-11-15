bd5be5f105161225f204c7a275005be57c657968
Rudro Saha <saharudro15@gmail.com>
2024-11-16 03:17:05 +0600

    Update apiSlice.js

--- src/store/features/apiSlice.js
+++ src/store/features/apiSlice.js
@@ -1,5 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+
 export const rootApi = createApi({
   reducerPath: "rootApi",
   baseQuery: fetchBaseQuery({baseUrl: `http://localhost:3000/`}),
